3aa89325075050fb4697a8c061d391fb011651a1
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-10-31 17:23:12 +0200

    .

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 
-print("{0}".format(''.join([chr(i) for i in range(ord('a'), ord('z') + 1)])), end='')
+for i in range(ord('a'), ord('z') + 1):
+    print("{0}".format(chr(i)), end='')
