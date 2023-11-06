686df0bee204f8ebf6a063a447cd49743e1c6c75
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-11-06 21:39:10 +0200

    .

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@
 
 def multiple_returns(sentence):
     # if len(sentence) == 0:
-        # return 0, None
-    return len(sentence), None if len(sentence) == 0 else sentence[0]
+    #     return 0, None
+    return (len(sentence), sentence[0]) if len(sentence) != 0 else (0, None)
