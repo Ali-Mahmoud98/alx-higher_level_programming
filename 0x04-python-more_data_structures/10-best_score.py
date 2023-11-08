09ccf3dd0fa43df2d03f2827a9fa26cd970ea1c5
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-11-08 20:14:17 +0200

    .

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max_key = list(a_dictionary.keys())[0]
+    max_val = list(a_dictionary.values())[0]
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+    for k, v in a_dictionary.items():
+        if v > max_val:
+            max_val = v
+            max_key = k
+    return max_key
