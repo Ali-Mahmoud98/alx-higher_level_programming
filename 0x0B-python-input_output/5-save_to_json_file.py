2e344cbaf8e14c5bc83e9681abf213796c54a769
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-12-06 07:09:28 +0200

    .

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a function writes an Object to a text file,
+using a JSON representation.
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """function that writes an Object to a text file,
+    using a JSON representation.
+
+    Args:
+        my_obj (Any): The object to be serialized to JSON.
+        filename (str): the file name.
+    """
+    jsonStr = json.dumps(my_obj)
+    with open(filename, mode="w", encoding="utf-8") as f:
+        f.write(jsonStr)
