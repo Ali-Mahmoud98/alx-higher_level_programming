5a7f84244e9c8242f74ed4989288f40e07093f87
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-12-06 14:15:14 +0200

    .

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines Class Student"""
+
+
+class Student:
+    """Student Class"""
+    def __init__(self, first_name, last_name, age):
+        """Initialization Method
+
+        Args:
+            first_name (str): student first name
+            last_name (str): student last name
+            age (int): student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            str : dictionary representation of Object.
+        """
+        return self.__dict__
