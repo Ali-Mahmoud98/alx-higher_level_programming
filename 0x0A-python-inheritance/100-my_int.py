4bd7c155f1009d99f2812cb35e27475c13bc4446
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-12-06 03:25:20 +0200

    .

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a class MyInt that inherits from int."""
+
+
+class MyInt(int):
+    """Class MyInt drived from int."""
+    def __eq__(self, val):
+        """Overrides == to !=."""
+        return self.real != val 
+
+    def __ne__(self, val):
+        """Overrides != to ==."""
+        return self.real == val 
