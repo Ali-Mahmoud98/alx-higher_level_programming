95ff8681d9d32ce0a6bf5d554e5428e7adadf2a0
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-12-11 23:15:58 +0200

    .

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -159,7 +159,7 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width, self.height)
-    
+
     def update(self, *args):
         """Update the Rectangle.
 
