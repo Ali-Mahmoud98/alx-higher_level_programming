780c88759dee9aa5439e7ee4027ee2527accb2a5
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-12-12 14:31:20 +0200

    .

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -70,3 +70,18 @@ class Base:
         if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set.
+
+        Args:
+            **dictionary (dict): Key/value pairs of attributes to initialize.
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                obj = cls(1, 1)
+            else:
+                obj = cls(1)
+            obj.update(**dictionary)
+            return obj
