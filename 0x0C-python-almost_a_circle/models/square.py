3bd529da62a9682024f6503f93ea2ef7d4005e80
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-12-12 12:48:23 +0200

    .

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This model defines Square class."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class Square that inherits from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Intialization of Square.
+        This method called when instanciating instance of Rectangle.
+
+        Args:
+            size (int): The size of the new Rectangle.
+            x (int, optional): The x coordinate of the new Square.
+                                Defaults to 0.
+            y (int, optional): The y coordinate of the new Square.
+                                Defaults to 0.
+            id (int, optional): The identity of the new Square.
+                                Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+    
+    @property
+    def size(self):
+        """Get the size of the Square"""
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        """Setting value to size.
+
+        Args:
+            value (int): the value to be assigned to size.
+        """
+        self.width = value
+        self.height = value
