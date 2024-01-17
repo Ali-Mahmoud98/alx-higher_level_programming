2d2a03701c36c3bc006b5e068cb8743cf0d979fb
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2024-01-17 04:37:29 +0200

    .

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- lists all the tables of a database in your MySQL server.
+-- when using command line you should pass database name.
+-- cat 3-list_tables.sql | mysql -hlocalhost -uroot -p 'database_name'.
+SHOW TABLES;
